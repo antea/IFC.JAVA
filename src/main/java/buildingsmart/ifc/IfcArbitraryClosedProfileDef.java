61ab17890550f5ebc950eaf1dcba11f57e70ea26
Giovanni Velludo <giovanni.velludo@studenti.unipd.it>
2020-09-08 19:44:53 +0200

    Adds IfcPolyline and IfcArbitraryClosedProfileDef.

--- src/main/java/buildingsmart/ifc/IfcArbitraryClosedProfileDef.java
+++ src/main/java/buildingsmart/ifc/IfcArbitraryClosedProfileDef.java
@@ -19,13 +19,79 @@
 
 package buildingsmart.ifc;
 
-public class IfcArbitraryClosedProfileDef extends IfcProfileDef {
-    private final IfcCurve OuterCurve;
+import buildingsmart.io.Attribute;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
 
-    public IfcArbitraryClosedProfileDef(IfcProfileTypeEnum profileType,
+/**
+ * The closed profile <i>IfcArbitraryClosedProfileDef</i> defines an arbitrary
+ * two-dimensional profile for the use within the swept surface geometry, the
+ * swept area solid or a sectioned spine. It is given by an outer boundary from
+ * which the surface or solid can be constructed. </p>
+ * <p><u>Informal
+ * proposition</u>: </p>
+ * <ol>
+ *   <li>The <i>OuterCurve</i>
+ * has to be a closed curve.</li>
+ *   <li>The <i>OuterCurve</i>
+ * shall not intersect.</li>
+ * </ol>
+ * <p><u>Position</u>
+ *       <br>
+ * The <i>OuterCurve</i>
+ * is defined in the underlying coordinate system. The underlying
+ * coordinate system is defined by the swept surface or swept area solid
+ * that uses the profile definition. It is the xy plane of either:</p>
+ *       <ul>
+ *         <li style="font-style: italic;">IfcSweptSurface.Position</li>
+ *         <li style="font-style: italic;">IfcSweptAreaSolid.Position</li>
+ *       </ul>
+ * or in case of sectioned spines the xy plane of each list member of <span
+ *  style="font-style: italic;">IfcSectionedSpine.CrossSectionPositions</span>
+ *       <p><u>Parameter</u>
+ *       <br>
+ * The <i>OuterCurve</i>
+ * attribute defines a two dimensional closed bounded curve.</p>
+ */
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class IfcArbitraryClosedProfileDef extends IfcProfileDef {
+    @Attribute(2)
+    private final IfcCurve outerCurve;
+
+    /**
+     * @param profileType Defines the type of geometry into which this profile
+     *                    definition shall be resolved, either a curve or a
+     *                    surface area. In case of curve the profile should be
+     *                    referenced by a swept surface, in case of area the
+     *                    profile should be referenced by a swept area solid.
+     * @param profileName Name of the profile type according to some standard
+     *                    profile table.
+     * @throws NullPointerException     If {@code profileType} or {@code
+     *                                  outerCurve} are null.
+     * @throws IllegalArgumentException If the dimensionality of {@code
+     *                                  outerCurve} is not 2, if {@code
+     *                                  outerCurve} is not of type IfcLine, if
+     *                                  {@code outerCurve} is not of type
+     *                                  IfcCurve.
+     */
+    public IfcArbitraryClosedProfileDef(@NonNull IfcProfileTypeEnum profileType,
                                         IfcLabel profileName,
-                                        IfcCurve outerCurve) {
+                                        @NonNull IfcCurve outerCurve) {
         super(profileType, profileName);
-        OuterCurve = outerCurve;
+        if (outerCurve.getDim().getValue() != 2) {
+            throw new IllegalArgumentException(
+                    "dimensionality of outerCurve must be 2");
+        }
+        if (outerCurve instanceof IfcLine) {
+            throw new IllegalArgumentException(
+                    "outerCurve cannot be of type IfcLine");
+        }
+        if (outerCurve instanceof IfcOffsetCurve2D) {
+            throw new IllegalArgumentException(
+                    "outerCurve cannot be of type IfcOffsetCurve2D");
+        }
+        this.outerCurve = outerCurve;
     }
 }
--- src/main/java/buildingsmart/ifc/IfcPolyline.java
+++ src/main/java/buildingsmart/ifc/IfcPolyline.java
@@ -19,8 +19,61 @@
 
 package buildingsmart.ifc;
 
+import buildingsmart.io.Attribute;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+import java.util.Arrays;
 import java.util.List;
 
-public abstract class IfcPolyline extends IfcBoundedCurve {
-    private List<IfcCartesianPoint> points;
+/**
+ * An IfcPolyline is a bounded curve of n -1 linear segments, defined by a list
+ * of n points, P0, P1 ... Pn-1.
+ */
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class IfcPolyline extends IfcBoundedCurve {
+    @Attribute(0)
+    private final List<IfcCartesianPoint> points;
+
+    /**
+     * @param points The points defining the polyline.
+     * @throws NullPointerException     If {@code points} is {@code null}.
+     * @throws IllegalArgumentException If the size of {@code points} is smaller
+     *                                  than 2, if all {@code points} don't have
+     *                                  the same dimensionality.
+     */
+    public IfcPolyline(@NonNull List<IfcCartesianPoint> points) {
+        if (points.size() < 2) {
+            throw new IllegalArgumentException(
+                    "size of points must be at least 2");
+        }
+        IfcDimensionCount firstPointDim = points.get(0).getDim();
+        if (!points.stream()
+                .allMatch(point -> point.getDim().equals(firstPointDim))) {
+            throw new IllegalArgumentException(
+                    "dimensionality of all points must be the same");
+        }
+        this.points = points;
+    }
+
+    /**
+     * @param points The points defining the polyline.
+     * @throws NullPointerException     If {@code points} is {@code null}.
+     * @throws IllegalArgumentException If the size of {@code points} is smaller
+     *                                  than 2, if all {@code points} don't have
+     *                                  the same dimensionality.
+     */
+    public IfcPolyline(@NonNull IfcCartesianPoint... points) {
+        this(Arrays.asList(points));
+    }
+
+    /**
+     * @return The space dimensionality of this IfcCurve.
+     */
+    @Override
+    public IfcDimensionCount getDim() {
+        return points.get(0).getDim();
+    }
 }
