ea94d29a719979c3a70bc15c41f9ece4a24d1752
Giovanni Velludo <giovanni.velludo@studenti.unipd.it>
2020-10-12 22:29:31 +0200

    Adds IfcEllipse.

--- src/main/java/buildingsmart/ifc/IfcEllipse.java
+++ src/main/java/buildingsmart/ifc/IfcEllipse.java
@@ -19,19 +19,68 @@
 
 package buildingsmart.ifc;
 
+import buildingsmart.io.Attribute;
+import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import lombok.ToString;
 
-public abstract class IfcEllipse extends IfcConic {
-    private IfcLengthMeasure SemiAxis1;
-    private IfcLengthMeasure SemiAxis2;
+/**
+ * <P><U>Definition from ISO/CD 10303-42:1992</U>: An ellipse (IfcEllipse) is
+ * a conic section defined by the lengths of the semi-major and semi-minor
+ * diameters and the position (center or mid point of the line joining the foci)
+ * and orientation of the curve. Interpretation of the data shall be as
+ * follows:
+ * </P>
+ * <BLOCKQUOTE>
+ * <BLOCKQUOTE>
+ * <PRE>C  = SELF\IfcConic.Position.Location </PRE>
+ * <PRE>x  = SELF\IfcConic.Position.P[1] </PRE>
+ * <PRE>y  = SELF\IfcConic.Position.P[2] </PRE>
+ * <PRE>z  = SELF\IfcConic.Position.P[3] </PRE>
+ * <PRE>R1 = SemiAxis1 </PRE>
+ * <PRE>R2 = SemiAxis2</PRE></BLOCKQUOTE></BLOCKQUOTE>
+ * <P>The parameterization range is 0 <FONT FACE="Symbol">&pound;</FONT>
+ * <I>u</I> <FONT FACE="Symbol">&pound;</FONT> 2<FONT
+ * FACE="Symbol">p</FONT> (or 0
+ * <FONT FACE="Symbol">&pound;</FONT> <I>u</I> <FONT
+ * FACE="Symbol">&pound;</FONT> 360 degree).</P>
+ * <P> The inherited Position.Location from IfcConic is the center of the
+ * IfcEllipse, and the inherited Position.P[1] from IfcConic the direction of
+ * the SemiAxis1. </P>
+ */
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class IfcEllipse extends IfcConic {
+    @Attribute(1)
+    private final IfcPositiveLengthMeasure semiAxis1;
+    @Attribute(2)
+    private final IfcPositiveLengthMeasure semiAxis2;
 
     /**
-     * @param position The location and orientation of the conic. Further
-     *                 details of the interpretation of this attribute are given
-     *                 for the individual subtypes.
-     * @throws NullPointerException If position is null.
+     * @param position  The location and orientation of the conic. Further
+     *                  details of the interpretation of this attribute are
+     *                  given for the individual subtypes.
+     * @param semiAxis1 The first radius of the ellipse which shall be positive.
+     *                  Its direction is the same of the x axis of {@code
+     *                  position}.
+     * @param semiAxis2 The second radius of the ellipse which shall be
+     *                  positive.
+     * @throws NullPointerException If position, semiAxis1 or semiAxis2 are
+     *                              null.
      */
-    public IfcEllipse(@NonNull IfcAxis2Placement position) {
+    public IfcEllipse(@NonNull IfcAxis2Placement position,
+                      @NonNull IfcPositiveLengthMeasure semiAxis1,
+                      @NonNull IfcPositiveLengthMeasure semiAxis2) {
         super(position);
+        this.semiAxis1 = semiAxis1;
+        this.semiAxis2 = semiAxis2;
+    }
+
+    /**
+     * @return The space dimensionality of this IfcCurve.
+     */
+    @Override
+    public IfcDimensionCount getDim() {
+        return getPosition().getDim();
     }
 }
